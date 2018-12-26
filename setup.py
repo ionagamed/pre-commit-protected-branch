b5d0ae5bcd3c39c39076f27e6da3bb68141abf39
Leonid Lygin <ionagamed@gmail.com>
2018-12-26 21:24:24 +0300

    fix build

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-	name='Pre-commit hook for protected branches',
-	entry_points={
-		'console_scripts': [
-			pre-commit-protected-branch = pre_commit_hooks.protected_branch:main
-		]
-	}
+    name='Pre-commit hook for protected branches',
+    entry_points={
+        'console_scripts': [
+            'pre-commit-protected-branch = pre_commit_hooks.protected_branch:main'
+        ]
+    }
 )
