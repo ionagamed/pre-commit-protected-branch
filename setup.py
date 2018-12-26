10050ab128a1e5237f21058f2ef1535bb3ad765f
Leonid Lygin <ionagamed@gmail.com>
2018-12-26 21:57:51 +0300

    naming

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Pre-commit hook for protected branches',
+    name='pre_commit_protected_branch',
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'pre-commit-protected-branch = pre_commit_protected_branch.protected_branch:main'
